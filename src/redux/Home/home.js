c584bad9bdb30b28d914d2a0f50e52397d65392d
Marco <marcooquendoc@gmail.com>
2023-03-09 14:14:13 +0000

    Feature register, login, logout, currentUser, fetchItems

--- src/redux/Home/home.js
+++ src/redux/Home/home.js
@@ -1,40 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../api/api';
 
-const URL = 'https://picsum.photos/200/300';
 const SHOW_ITEMS = 'SHOW_ITEMS';
 
-const fetchItemsInfo = createAsyncThunk(SHOW_ITEMS, async () => {
-  const response = await fetch(URL);
-  const images = await response.json();
-  const imagesInfo = images.map(({
-    // eslint-disable-next-line camelcase
-    id, author, width, height, url, download_url,
-  }) => ({
-    id: parseInt(id, 10),
-    author,
-    width,
-    height,
-    url,
-    // eslint-disable-next-line camelcase
-    downloadUrl: download_url,
-    picsumUrl: `${baseUrl}${id}${size}`,
-    nosizeUrl: `${baseUrl}${id}`,
-    blurUrl: `${baseUrl}${id}${size}?blur`,
-    grayUrl: `${baseUrl}${id}${size}?grayscale`,
-    grayBlurUrl: `${baseUrl}${id}${size}?grayscale&blur`,
-  }));
-  return imagesInfo;
+const initialState = [];
+
+// Thunk
+const getItemsInfo = createAsyncThunk(SHOW_ITEMS, async () => {
+  try {
+    return await api.fetchItems();
+  } catch (error) {
+    return error.message;
+  }
 });
 
-const initialState = [];
-export const imageSlice = createSlice({
-  name: 'image',
+// reducer
+export const itemSlice = createSlice({
+  name: 'item',
   initialState,
   reducers: { },
   extraReducers: {
-    [fetchItemsInfo.fulfilled]: (state, action) => action.payload,
+    [getItemsInfo.fulfilled]: (state, action) => action.payload,
   },
 });
 
-export { fetchItemsInfo };
-export default imageSlice.reducer;
+export { getItemsInfo };
+export default itemSlice.reducer;
