dea858377f799622b4da25027b928a1aa7679891
Marco Oquendo <marcooquendoc@gmail.com>
2023-03-09 15:29:12 -0500

    Get items from api to home page and display them

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getItemsInfo } from '../redux/Home/home';
 
 const Home = () => {
   const dispatch = useDispatch();
-
+  let items = [];
   const handleGetItemsInfo = () => {
     dispatch(getItemsInfo());
   };
+
+  useEffect(() => {
+    handleGetItemsInfo();
+  }, []);
+
+  items = useSelector((store) => store.items);
 
   return (
     <div>
       <h1>Home</h1>
       <div>
         Items:
+        {items[0]
+          ? items.map((item) => (
+            <div key={item.id}>
+              <p>{item.title}</p>
+            </div>
+          )) : null}
         <br />
       </div>
       <br />
