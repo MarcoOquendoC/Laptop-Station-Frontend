59b5f999abe63b55208066f51111805a38ab06c1
Mauricio Mendoza <rbreva@gmail.com>
2023-03-10 18:27:28 -0500

    configuring images and menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getItemsInfo } from '../redux/Home/home';
+import menuImg from '../images/menu.svg';
+import logo from '../images/logo_laptop_station.svg';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,9 +20,28 @@ const Home = () => {
   const items = useSelector((store) => store.items);
 
   return (
-    <div>
-      <h1>Home</h1>
-      <div>
+    <>
+      <div className="principal">
+        <div className="header">
+          <div className="menu mob"><img src={menuImg} alt="menu" /></div>
+          <div className="menu_logo">
+            <img className="logo" src={logo} alt="logo" />
+          </div>
+          <div className="menu mob" />
+        </div>
+
+        <nav className="menunav">
+          <ul className="menunav_ul">
+            <li className="menu_text select"><Link to="/">Home</Link></li>
+            <li className="menu_text"><Link to="/laptops">Latops</Link></li>
+            <li className="menu_text"><Link to="/reserves">Reserves</Link></li>
+            <li className="menu_text admin"><Link to="/addItem">Add item</Link></li>
+            <li className="menu_text"><Link to="/logOut">Log Out</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="viewfinder">
         Items:
         {items[0]
           ? items.map((item) => (
@@ -29,9 +51,7 @@ const Home = () => {
           )) : null}
         <br />
       </div>
-      <br />
-      <button type="button" onClick={handleGetItemsInfo}>items</button>
-    </div>
+    </>
   );
 };
 
