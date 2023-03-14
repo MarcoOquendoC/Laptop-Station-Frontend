0e185d7b85bcbbdf05b92851ea434bd2e0765f3c
Josue Benavides <jdbs9514@gmail.com>
2023-03-14 17:36:53 -0500

    Add route to register and session components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
 import Home from './components/Home';
 import Detail from './components/Detail';
 import Reserve from './components/Reserve';
+import Session from './components/Session';
+import Registration from './components/Registration';
 
 function App() {
   const location = useLocation();
@@ -12,6 +14,8 @@ function App() {
   return (
     <div className="container">
       <Routes>
+        <Route path="/signin" element={<Session />} />
+        <Route path="/signup" element={<Registration />} />
         <Route path="/" element={<Home />} />
         <Route path="/detail" element={<Detail id={id} />} />
         <Route path="/detail/id/reserve" element={<Reserve />} />
