5108bf5f8d790cdd3905bc717abde726f1be1a85
Josue Benavides <jdbs9514@gmail.com>
2023-03-13 23:02:39 -0500

    add registration component

---
+++ src/components/Registration.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, Navigate } from 'react-router-dom';
+
+const Registration = () => {
+
+
+  return (
+    <>
+      <div>
+        <form action="">
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+          <button type="button">sign in</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Registration;
